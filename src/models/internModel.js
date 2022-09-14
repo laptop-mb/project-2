68eeab9e0ccb28d1437d64488267ba83de1df25c
laptop-mb <bansalmonika143@gmail.com>
2022-09-14 12:16:35 +0530

    done

--- src/models/internModel.js
+++ src/models/internModel.js
@@ -1,6 +1,16 @@
 const mongoose = require('mongoose');
+const ObjectId = mongoose.Schema.Types.ObjectId
 
-const collegeSchema = new mongoose.Schema({
+// { name: {mandatory}, email: {mandatory, valid email, unique}, mobile: {mandatory, valid mobile number, unique}, collegeId: {ObjectId, ref to college model, isDeleted: {boolean, default: false}}
+
+const internSchema = new mongoose.Schema({
+    name: {type: String, required:true},
+    email: {type: String, required: true, uniuqe: true},
+    mobile:{tyep:String, required: true, uniuqe: true, },
+    collegeId: {type: ObjectId, ref:("collegename"),  },
+    isDeleted: {type: Boolean, default: false}
+
+},{timestamps: true})
    
 
-module.exports = mongoose.model('collegename', collegeSchema)
+module.exports = mongoose.model('internname', internSchema)
